e8882d18540e7c291814df94a084ebd3c65edf0d
root <root@bulldog.(none)>
2017-07-03 09:25:07 +0700

    added 1st script file ever

---
+++ tris_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def tris_shownetwork():
+	subprocess.call("cat /etc/sysconfig/network-scripts/ifcfg-eth0",shell=True)
+
+
+def singing():
+	for i in range(5):
+		print "La la la"
+def math():
+	sum = 1+1
+	print "%s"%sum
+
+def diskusage():
+	du = subprocess.call(["df","-h"])
+	print "%s"%du
+
+def main():
+	tris_shownetwork()
+	singing()
+	math()	
+	diskusage()
+
+#main()
+
+
+
+if __name__ == "__main__":
+	main()	
